a1ab9aff5d8d6d2df7b75e29ab772f63accb4d8a
ts <tristansetha@gmail.com>
2024-02-01 09:35:55 -0800

    after

--- src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
+++ src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.frankmoley.lil.fid.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 @Component
 @Aspect
@@ -20,8 +22,8 @@ public class LoggingAspect {
     public void executeLogging() {
     }
 
-    @Before("executeLogging()")
-    public void logMethodCall(JoinPoint joinPoint) {
+    @AfterReturning(value = "executeLogging()", returning = "returnValue")
+    public void logMethodCall(JoinPoint joinPoint, Object returnValue) {
         StringBuilder message = new StringBuilder("Method: ");
         message.append(joinPoint.getSignature().getName());
         Object[] args = joinPoint.getArgs();
@@ -31,6 +33,11 @@ public class LoggingAspect {
                 message.append(arg).append(" | ");
             });
             message.append("]");
+        }
+        if (returnValue instanceof Collection) {
+            message.append(", returning").append(((Collection) returnValue).size()).append(" instances(s)");
+        } else {
+            message.append(", returning: ").append(returnValue.toString());
         }
         LOGGER.info(message.toString());
     }
