2f39637c838896370d35051e5c3d844efd646954
ts <tristansetha@gmail.com>
2024-01-31 16:41:30 -0800

    complete

--- src/main/java/com/frankmoley/lil/fid/config/ApplicationConfig.java
+++ src/main/java/com/frankmoley/lil/fid/config/ApplicationConfig.java
@@ -1,53 +1,12 @@
 package com.frankmoley.lil.fid.config;
 
-import com.frankmoley.lil.fid.service.GreetingService;
-import com.frankmoley.lil.fid.service.OutputService;
-import com.frankmoley.lil.fid.service.TimeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-
-import java.sql.Time;
 
 @Configuration
 @PropertySource("classpath:application.properties")
+@ComponentScan(basePackages =  "com.frankmoley.lil.fid")
 public class ApplicationConfig {
-    @Value("${app.greeting}")
-    private String greeting;
-    @Value("${app.name}")
-    private String name;
 
-    @Value("#{new Boolean(environment['spring.profiles.active'] != 'dev')}")
-    private boolean is24;
-
-    @Autowired
-    private GreetingService greetingService;
-    @Autowired
-    private TimeService timeService;
-
-
-    @Bean
-//    @Profile("!dev")
-    public TimeService timeService() {
-        return new TimeService(is24);
-    }
-
-//    @Bean
-//    @Profile("dev")
-//    public TimeService timeService12() {
-//        return new TimeService(false);
-//    }
-
-    @Bean
-    public OutputService outputService() {
-        return new OutputService(greetingService, timeService, name);
-    }
-
-    @Bean
-    public GreetingService greetingService() {
-        return new GreetingService(greeting);
-    }
 }
