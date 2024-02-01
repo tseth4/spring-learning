47484f9147518a4b59cba94f6b0341cfe8b60812
ts <tristansetha@gmail.com>
2024-02-01 09:33:31 -0800

    before

--- src/main/java/com/frankmoley/lil/fid/config/ApplicationConfig.java
+++ src/main/java/com/frankmoley/lil/fid/config/ApplicationConfig.java
@@ -2,11 +2,14 @@ package com.frankmoley.lil.fid.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages =  "com.frankmoley.lil.fid")
+// allows aspect proxy to load
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
 }
